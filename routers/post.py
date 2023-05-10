a2fee5e2397c655c864f0883048b24a2a16223ea
ishud <ishudev2021@gmail.com>
2023-05-10 22:05:45 +0530

    get method for db and router

--- routers/post.py
+++ routers/post.py
@@ -12,3 +12,9 @@ router = APIRouter(
 @router.post('') # passing empty string bcoz router prefix is /post already
 def create(request: PostBase, db: Session = Depends(get_db)):
     return db_post.create(db,request)
+
+@router.get('/all')
+def get_all_posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
+
+
