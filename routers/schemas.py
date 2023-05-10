fa405dde76e013ab32d9ffa5f734efb8da49fa27
ishud <ishudev2021@gmail.com>
2023-05-10 20:11:59 +0530

    schemas created

--- routers/schemas.py
+++ routers/schemas.py
@@ -1,1 +1,20 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class PostBase(BaseModel): # this schema shows what you can extract from a post api request
+    title: str
+    content: str
+    image_url: str
+    creator: str
+
+class PostDisplay(BaseModel): #  this schema displays how the PostBase information is stored in the api
+    id: int
+    title: str
+    content: str
+    image_url: str
+    creator: str
+    timestamp: datetime
+    class Config(): # we define this here so that the stuff in database gets mapped with this format
+        # ORM - Object Relational Mapping
+        orm_mode = True
 
