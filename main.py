ad1f66f94c24d43a2d0eb4ad33d8576c78b2b8f0
ishud <ishudev2021@gmail.com>
2023-05-10 23:38:59 +0530

    small error in main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins = origins,
-    allo_credentials = True,
+    allow_credentials = True,
     allow_methods = ['*'],
     allow_headers = ['*']
 )
